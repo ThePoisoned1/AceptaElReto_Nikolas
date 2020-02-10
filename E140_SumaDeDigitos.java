4024e3d63830dda093472434dbada7cc6a0ced23
ThePoisoned1 <60658745+ThePoisoned1@users.noreply.github.com>
2020-02-11 00:03:54 +0100

    Create E140_SumaDeDigitos.java

---
+++ E140_SumaDeDigitos.java
@@ -0,0 +1,41 @@
+package AceptaElReto.AC;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author ThePoisoned1
+ */
+public class E140_SumaDeDigitos {
+
+    Scanner sc;
+
+    public boolean caso() {
+        String numero = sc.nextLine();
+        if (Long.parseLong(numero) < 0) {
+            return false;
+        }
+       int suma = 0;
+        for (int i = 0; i < numero.length(); i++) {
+            if (i == numero.length() - 1) {
+                System.out.print(numero.charAt(i) + " = ");
+            } else {
+                System.out.print(numero.charAt(i) + " + ");
+            }
+            suma = suma + Character.getNumericValue(numero.charAt(i));
+        }
+        System.out.println(suma);
+        return true;
+    }
+
+    public void run() {
+        sc = new Scanner(System.in);
+        while (caso()) {
+        }
+    }
+
+    public static void main(String[] args) {
+        new E140_SumaDeDigitos().run();
+    }
+
+}
