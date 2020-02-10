ae9c8507520c2f5111b128671809574a89d6d03c
ThePoisoned1 <60658745+ThePoisoned1@users.noreply.github.com>
2020-02-10 21:21:46 +0100

    Create E116_HolaMundo.java

---
+++ E116_HolaMundo.java
@@ -0,0 +1,18 @@
+package AceptaElReto.AC;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author ThePoisoned1
+ */
+public class E116_HolaMundo {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int in = Integer.parseInt(sc.nextLine());
+        for (int i = 0; i < in; i++) {
+            System.out.println("Hola mundo.");
+        }
+    }
+}
