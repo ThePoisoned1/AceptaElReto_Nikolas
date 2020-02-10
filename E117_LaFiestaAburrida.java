dbc707bccd59fe06c4ba3ce9035a8256b5fc2318
ThePoisoned1 <60658745+ThePoisoned1@users.noreply.github.com>
2020-02-10 21:26:35 +0100

    Create E117_LaFiestaAburrida.java

---
+++ E117_LaFiestaAburrida.java
@@ -0,0 +1,32 @@
+package AceptaElReto.AC;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author ThePoisoned1
+ */
+public class E117_LaFiestaAburrida {
+
+    Scanner sc;
+
+    public void caso() {
+        String[] input = sc.nextLine().trim().split(" ");
+
+        if (input[0].equals("Soy") || (input[0].equals("soy"))) {
+            System.out.println("Hola, " + input[1] + ".");
+        }
+    }
+
+    public void run() {
+        sc = new Scanner(System.in);
+        int veces = Integer.parseInt(sc.nextLine());
+        for (int i = 0; i < veces; i++) {
+            caso();
+        }
+    }
+
+    public static void main(String[] args) {
+      new E117_LaFiestaAburrida().run();
+    }
+}
