b99ac43b641338b553f570e44f7a763bcf6a674b
ThePoisoned1 <60658745+ThePoisoned1@users.noreply.github.com>
2020-02-10 21:13:30 +0100

    Create E112_RadaresDeTramo.java

---
+++ E112_RadaresDeTramo.java
@@ -0,0 +1,44 @@
+package AceptaElReto;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author ThePoisoned1
+ */
+public class E112_RadaresDeTramo {
+
+    Scanner sc;
+
+    boolean caso() {
+        int tramo = sc.nextInt();
+        int velocidadMaxima = sc.nextInt();
+        int tiempoTarda = sc.nextInt();
+        if (tramo == 0 && velocidadMaxima == 0 && tiempoTarda == 0) {
+            return false;
+        }
+        if (tramo <= 0 || velocidadMaxima<=0 || tiempoTarda <= 0) {
+            System.out.println("ERROR");
+            return true;
+        }
+        double velocidadMedia = (double) tramo / tiempoTarda;
+        velocidadMedia *= 3.6;
+        if (velocidadMedia >= velocidadMaxima) {
+            System.out.println(velocidadMedia >= velocidadMaxima * 1.2 ? "PUNTOS" : "MULTA");
+
+        } else {
+            System.out.println("OK");
+        }
+        return true;
+    }
+
+    void run() {
+        sc = new Scanner(System.in);
+        while (caso()) {
+        }
+    }
+
+    public static void main(String[] args) {
+        new E112_RadaresDeTramo().run();
+    }
+}
