329b1ae53c93bbae39515f11ce4abb4b04c3a951
ThePoisoned1 <60658745+ThePoisoned1@users.noreply.github.com>
2020-02-11 02:17:00 +0100

    Create E155_PerimetroDeUnRectangulo.java

---
+++ E155_PerimetroDeUnRectangulo.java
@@ -0,0 +1,32 @@
+package AceptaElReto;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author ThePoisoned1
+ */
+public class E155_PerimetroDeUnRectangulo {
+
+    Scanner sc;
+
+    public boolean caso() {
+        int ladoX = sc.nextInt();
+        int ladoY = sc.nextInt();
+        if (ladoX < 0 || ladoY < 0) {
+            return false;
+        }
+        System.out.println((ladoX*2 + ladoY*2));
+        return true;
+    }
+
+    public void run() {
+        sc = new Scanner(System.in);
+        while (caso()) {
+        }
+    }
+
+    public static void main(String[] args) {
+        new E155_PerimetroDeUnRectangulo().run();
+    }
+}
