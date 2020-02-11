9df0c1dd78ffe52712f751fdd96f387fd0c39a8b
ThePoisoned1 <60658745+ThePoisoned1@users.noreply.github.com>
2020-02-11 13:42:51 +0100

    Create E237_NumerosPolidivisibles.java

---
+++ E237_NumerosPolidivisibles.java
@@ -0,0 +1,43 @@
+package AceptaElReto.AC;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author ThePoisoned1
+ */
+public class E237_NumerosPolidivisibles {
+
+    Scanner sc;
+
+    public boolean caso() {
+        if (!sc.hasNextLine()) {
+            return false;
+        }
+        String num = sc.nextLine();
+        String aux;
+
+        for (int i = 1; i <= num.length(); i++) {
+            aux = "";
+            for (int j = 0; j < i; j++) {
+                aux = aux + num.charAt(j);
+            }
+            if (Long.parseLong(aux) % i != 0) {
+                System.out.println("NO POLIDIVISIBLE");
+                return true;
+            }
+        }
+        System.out.println("POLIDIVISIBLE");
+        return true;
+    }
+
+    public void run() {
+        sc = new Scanner(System.in);
+        while (caso()) {
+        }
+    }
+
+    public static void main(String[] args) {
+        new E237_NumerosPolidivisibles().run();
+    }
+}
