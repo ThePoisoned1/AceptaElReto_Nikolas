953249d7fa5c02792fe4aa3d58999e2b059d2099
ThePoisoned1 <60658745+ThePoisoned1@users.noreply.github.com>
2020-02-11 02:32:47 +0100

    Create E165_NumeroHyperPar.java

---
+++ E165_NumeroHyperPar.java
@@ -0,0 +1,37 @@
+package AceptaElReto.AC;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author ThePoisoned1
+ */
+public class E165_NumeroHyperPar {
+
+    Scanner sc;
+
+    public boolean caso() {
+        String numero = sc.nextLine();
+        if (Integer.parseInt(numero) < 0) {
+            return false;
+        }
+        for (int i = 0; i < numero.length(); i++) {
+            if (Character.getNumericValue(numero.charAt(i)) % 2 != 0) {
+                System.out.println("NO");
+                return true;
+            }
+        }
+         System.out.println("SI");
+        return true;
+    }
+
+    public void run() {
+        sc = new Scanner(System.in);
+        while (caso()) {
+        }
+    }
+
+    public static void main(String[] args) {
+        new E165_NumeroHyperPar().run();
+    }
+}
