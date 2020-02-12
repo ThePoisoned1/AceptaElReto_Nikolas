f78a7f2264a90e327e7e8604cb1303d65133c4fd
ThePoisoned1 <60658745+ThePoisoned1@users.noreply.github.com>
2020-02-12 21:19:24 +0100

    Create E290_AlturaDeUnArbolBinario.java

---
+++ E290_AlturaDeUnArbolBinario.java
@@ -0,0 +1,58 @@
+package AceptaElReto.AC;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author ThePoisoned1
+ */
+public class E290_AlturaDeUnArbolBinario {
+
+    Scanner sc;
+    int pos;
+    String arbol;
+
+    int hallarAltura() {
+        int aux1 = 0, aux2 = 0;
+
+        if (pos < arbol.length() && arbol.charAt(pos) == '*') {
+            pos++;
+            aux1 = hallarAltura() + 1;
+        } else {
+            pos++;
+        }
+        if (pos < arbol.length() && arbol.charAt(pos) == '*') {
+            pos++;
+            aux2 = hallarAltura() + 1;
+        } else {
+            pos++;
+        }
+        if (aux1 > aux2) {
+            return aux1;
+        } else {
+            return aux2;
+        }
+    }
+
+    void caso() {
+        arbol = sc.next();
+        pos = 0;
+        int hMax = 0;
+        if (arbol.charAt(0) == '*') {
+            hMax += hallarAltura();
+        }
+        System.out.println(hMax);
+    }
+
+    void run() {
+        sc = new Scanner(System.in);
+        int veces = sc.nextInt();
+        for (int i = 0; i < veces; i++) {
+            caso();
+        }
+    }
+
+    public static void main(String[] args) {
+        new E290_AlturaDeUnArbolBinario().run();
+    }
+}
