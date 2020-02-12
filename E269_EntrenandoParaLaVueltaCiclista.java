a98d90c69af08fdc71e7270ea03b40ec640120e8
ThePoisoned1 <60658745+ThePoisoned1@users.noreply.github.com>
2020-02-12 20:56:30 +0100

    Create E269_EntrenandoParaLaVueltaCiclista.java

---
+++ E269_EntrenandoParaLaVueltaCiclista.java
@@ -0,0 +1,39 @@
+package AceptaElReto.AC;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author ThePoisoned1
+ */
+public class E269_EntrenandoParaLaVueltaCiclista {
+
+    Scanner sc;
+
+    void caso() {
+        String[] distancias = sc.nextLine().split(" ");
+        int dist = 0;
+        int aux = 0;
+        for (String i : distancias) {
+            if (!i.equals("0")) {
+                aux += Integer.parseInt(i);
+                dist = dist + 2 * aux;
+            }
+        }
+        System.out.println(dist);
+
+    }
+
+    void run() {
+        sc = new Scanner(System.in);
+        int veces = sc.nextInt();
+        sc.nextLine();
+        for (int i = 0; i < veces; i++) {
+            caso();
+        }
+    }
+
+    public static void main(String[] args) {
+        new E269_EntrenandoParaLaVueltaCiclista().run();
+    }
+}
