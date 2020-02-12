51197b3b2e33aa87f93aaf28c70ce7bebb7debd5
ThePoisoned1 <60658745+ThePoisoned1@users.noreply.github.com>
2020-02-12 20:46:27 +0100

    Create E515_PoniendoLaMesa.java

---
+++ E515_PoniendoLaMesa.java
@@ -0,0 +1,41 @@
+package AceptaElReto;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author 1b_dam20_22
+ */
+public class E515_PoniendoLaMesa {
+
+    Scanner sc;
+
+    boolean caso() {
+        int numCopas = sc.nextInt();
+        if (numCopas == 0) {
+            return false;
+        }
+        long contador = 0;
+        long viajes = 0;
+        while (numCopas > 0) {
+            if (numCopas % 2 == 0 && contador % 2 != 0) {
+                numCopas--;
+            }
+            numCopas--;
+            viajes++;
+            contador++;
+        }
+        System.out.println(viajes);
+        return true;
+    }
+
+    void run() {
+        sc = new Scanner(System.in);
+        while (caso()) {
+        }
+    }
+
+    public static void main(String[] args) {
+        new E515_PoniendoLaMesa().run();
+    }
+}
