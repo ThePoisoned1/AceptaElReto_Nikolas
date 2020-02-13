a70af9c99d7948f237b042b995908de50c6cb1fe
ThePoisoned1 <60658745+ThePoisoned1@users.noreply.github.com>
2020-02-13 16:36:29 +0100

    Create E295_Elevame.java

---
+++ E295_Elevame.java
@@ -0,0 +1,35 @@
+package AceptaElReto.AC;
+
+import java.util.Scanner;
+import java.math.BigInteger;
+
+/**
+ *
+ * @author ThePoisoned1
+ */
+public class E295_Elevame {
+
+    Scanner sc;
+    final BigInteger mod = BigInteger.valueOf(31543);
+    final BigInteger zero = BigInteger.ZERO;
+
+    boolean caso() {
+        BigInteger numero = sc.nextBigInteger();
+        BigInteger potencia = sc.nextBigInteger();
+        if (numero.equals(zero) && potencia.equals(zero)) {
+            return false;
+        }
+        System.out.println(numero.modPow(potencia, mod));
+        return true;
+    }
+
+    void run() {
+        sc = new Scanner(System.in);
+        while (caso()) {
+        }
+    }
+
+    public static void main(String[] args) {
+        new E295_Elevame().run();
+    }
+}
