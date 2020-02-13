915663c1d3ac9da3dde7e83f862825aedbbda809
ThePoisoned1 <60658745+ThePoisoned1@users.noreply.github.com>
2020-02-13 16:25:39 +0100

    Create E293_Artropodos.java

---
+++ E293_Artropodos.java
@@ -0,0 +1,32 @@
+package AceptaElReto.AC;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author ThePoisoned1
+ */
+public class E293_Artropodos {
+
+    Scanner sc;
+
+    void caso() {
+        System.out.println(sc.nextInt() * 6
+                + sc.nextInt() * 8
+                + sc.nextInt() * 10
+                + sc.nextInt() * 2 * sc.nextInt());
+    }
+
+    void run() {
+        sc = new Scanner(System.in);
+        int veces = sc.nextInt();
+        for (int i = 0; i < veces; i++) {
+            caso();
+        }
+    }
+
+    public static void main(String[] args) {
+        new E293_Artropodos().run();
+    }
+
+}
