7bc29c5449a7faa43a8264594de19b26c633d8d7
ThePoisoned1 <60658745+ThePoisoned1@users.noreply.github.com>
2020-02-14 17:41:47 +0100

    Create E512_DonerSospechoso.java

---
+++ E512_DonerSospechoso.java
@@ -0,0 +1,30 @@
+package AceptaElReto.AC;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author ThePoisoned1
+ */
+public class E512_DonerSospechoso {
+
+    Scanner sc;
+
+    void caso() {
+        int kilosConejo = sc.nextInt();
+        int kilosCaballo = sc.nextInt();
+        System.out.println((int) kilosConejo * 100 / (kilosConejo + kilosCaballo));
+    }
+
+    void run() {
+        sc = new Scanner(System.in);
+        int veces = sc.nextInt();
+        for (int i = 0; i < veces; i++) {
+            caso();
+        }
+    }
+
+    public static void main(String[] args) {
+        new E512_DonerSospechoso().run();
+    }
+}
