ff60d307ff8ee288b9e0642673c08329de1b425e
ThePoisoned1 <60658745+ThePoisoned1@users.noreply.github.com>
2020-02-14 15:10:46 +0100

    Create E427_YoSoyTu.java

---
+++ E427_YoSoyTu.java
@@ -0,0 +1,35 @@
+package AceptaElReto.AC;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author ThePoisoned1
+ */
+public class E427_YoSoyTu {
+
+    Scanner sc;
+
+    void caso() {
+        String persona = sc.nextLine();
+        String parentesco = sc.nextLine();
+        if (persona.equals("Luke") && parentesco.equals("padre")) {
+            System.out.println("TOP SECRET");
+        } else {
+            System.out.println(persona + ", yo soy tu " + parentesco);
+        }
+    }
+
+    void run() {
+        sc = new Scanner(System.in);
+        int veces = sc.nextInt();
+        sc.nextLine();
+        for (int i = 0; i < veces; i++) {
+            caso();
+        }
+    }
+
+    public static void main(String[] args) {
+        new E427_YoSoyTu().run();
+    }
+}
