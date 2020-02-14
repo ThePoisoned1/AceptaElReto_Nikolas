d2c170350b33522b59d2346bd8b472f6dc5c9c09
ThePoisoned1 <60658745+ThePoisoned1@users.noreply.github.com>
2020-02-14 17:42:27 +0100

    Create E513_EmpleadoDelAnyo.java

---
+++ E513_EmpleadoDelAnyo.java
@@ -0,0 +1,46 @@
+package AceptaElReto;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author ThePoisoned1
+ */
+public class E513_EmpleadoDelAnyo {
+
+    Scanner sc;
+
+    public void caso() {
+        int numDias = sc.nextInt();
+        sc.nextLine();
+        long media;
+        long suma = 0;
+        long trabajosDia;
+        for (long i = 1; i <= numDias; i++) {
+            if (i == 1) {
+                media = sc.nextInt();
+                suma = media;
+                trabajosDia = media;
+                System.out.print(trabajosDia);
+            } else {
+                media = sc.nextInt();
+                trabajosDia = (i * media) - suma;
+                suma += trabajosDia;
+                System.out.print(" " + trabajosDia);
+            }
+        }
+        sc.nextLine();
+        System.out.println("");
+    }
+
+    public void run() {
+        sc = new Scanner(System.in);
+        while (sc.hasNextLine()) {
+            caso();
+        }
+    }
+
+    public static void main(String[] args) {
+        new E513_EmpleadoDelAnyo().run();
+    }
+}
