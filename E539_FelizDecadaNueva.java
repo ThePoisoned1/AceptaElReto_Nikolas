f0ceaf0431c4ee9abc8d73ed1831413c9f1e44aa
ThePoisoned1 <60658745+ThePoisoned1@users.noreply.github.com>
2020-02-14 18:26:11 +0100

    Create E539_FelizDecadaNueva.java

---
+++ E539_FelizDecadaNueva.java
@@ -0,0 +1,36 @@
+package AceptaElReto.AC;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author ThePoisoned1
+ */
+public class E539_FelizDecadaNueva {
+
+    Scanner sc;
+
+    boolean caso() {
+        if (!sc.hasNextLine()) {
+            return false;
+        }
+         int periodo = Math.abs(sc.nextInt() - sc.nextInt());
+         sc.nextLine();
+            if ((periodo + 1) % 10 == 0) {
+                System.out.println("FELIZ DECADA NUEVA");
+            } else {
+                System.out.println("TOCA ESPERAR");
+            }
+        return true;
+    }
+
+    void run() {
+        sc = new Scanner(System.in);
+        while (caso()) {
+        }
+    }
+
+    public static void main(String[] args) {
+        new E539_FelizDecadaNueva().run();
+    }
+}
