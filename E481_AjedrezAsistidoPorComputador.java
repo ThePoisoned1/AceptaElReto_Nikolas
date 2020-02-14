a183f3a14000283a468e4097aa303d64ca81b1c7
ThePoisoned1 <60658745+ThePoisoned1@users.noreply.github.com>
2020-02-14 17:29:06 +0100

    Create E481_AjedrezAsistidoPorComputador.java

---
+++ E481_AjedrezAsistidoPorComputador.java
@@ -0,0 +1,59 @@
+package AceptaElReto.AC;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author ThePoisoned1
+ */
+public class E481_AjedrezAsistidoPorComputador {
+
+    Scanner sc;
+
+    boolean caso() {
+        int letra = sc.nextInt();
+        int numero = sc.nextInt();
+        if (letra == 0 && numero == 0) {
+            return false;
+        }
+        switch (letra) {
+            case 1:
+                System.out.print("h");
+                break;
+            case 2:
+                System.out.print("g");
+                break;
+            case 3:
+                System.out.print("f");
+                break;
+            case 4:
+                System.out.print("e");
+                break;
+            case 5:
+                System.out.print("d");
+                break;
+            case 6:
+                System.out.print("c");
+                break;
+            case 7:
+                System.out.print("b");
+                break;
+            case 8:
+                System.out.print("a");
+                break;
+
+        }
+        System.out.println(numero);
+        return true;
+    }
+
+    void run() {
+        sc = new Scanner(System.in);
+        while (caso()) {
+        }
+    }
+
+    public static void main(String[] args) {
+        new E481_AjedrezAsistidoPorComputador().run();
+    }
+}
