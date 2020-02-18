0e5ebf96454ddc8001e755aa28e4ed62dfb4826d
ThePoisoned1 <60658745+ThePoisoned1@users.noreply.github.com>
2020-02-18 17:45:58 +0100

    Create E141_ParentesisBalanceados.java

---
+++ E141_ParentesisBalanceados.java
@@ -0,0 +1,69 @@
+package AceptaElReto.SinEnviar;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author ThePoisoned1
+ */
+public class E141_ParentesisBalanceados {
+
+    Scanner sc;
+
+    public boolean caso() {
+        if (!sc.hasNextLine()) {
+            return false;
+        }
+        String linea = sc.nextLine();
+        StringBuilder parentesis = new StringBuilder();
+        for (int i = 0; i < linea.length(); i++) {
+            switch (linea.charAt(i)) {
+                case '(':
+                case '[':
+                case '{':
+                    parentesis.append(linea.charAt(i));
+                    break;
+                case ')':
+                    if (parentesis.length() != 0 && parentesis.charAt(parentesis.length() - 1) == '(') {
+                        parentesis.deleteCharAt(parentesis.length() - 1);
+                    } else {
+                        System.out.println("NO");
+                        return true;
+                    }
+                    break;
+                case ']':
+                    if (parentesis.length() != 0 && parentesis.charAt(parentesis.length() - 1) == '[') {
+                        parentesis.deleteCharAt(parentesis.length() - 1);
+                    } else {
+                        System.out.println("NO");
+                        return true;
+                    }
+                    break;
+                case '}':
+                    if (parentesis.length() != 0 && parentesis.charAt(parentesis.length() - 1) == '{') {
+                        parentesis.deleteCharAt(parentesis.length() - 1);
+                    } else {
+                        System.out.println("NO");
+                        return true;
+                    }
+                    break;
+            }
+        }
+        if (parentesis.length() != 0) {
+            System.out.println("NO");
+            return true;
+        }
+        System.out.println("YES");
+        return true;
+    }
+
+    public void run() {
+        sc = new Scanner(System.in);
+        while (caso()) {
+        }
+    }
+
+    public static void main(String[] args) {
+        new E141_ParentesisBalanceados().run();
+    }
+}
