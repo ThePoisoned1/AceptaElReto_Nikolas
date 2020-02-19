74d88ad783fd6b4d9481e21c985f8318751797b4
ThePoisoned1 <60658745+ThePoisoned1@users.noreply.github.com>
2020-02-19 16:59:12 +0100

    Create E355_GregorioXII.java

---
+++ E355_GregorioXII.java
@@ -0,0 +1,41 @@
+package AceptaElReto;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author ThePoisoned1
+ */
+public class E355_GregorioXII {
+
+    Scanner sc;
+
+    void caso() {
+        int anyo = sc.nextInt();
+        if (anyo % 4 == 0) {
+            if (anyo % 100 == 0) {
+                if (anyo % 400 == 0) {
+                    System.out.println("29");
+                } else {
+                    System.out.println("28");
+                }
+            } else {
+                System.out.println("29");
+            }
+        } else {
+            System.out.println("28");
+        }
+    }
+
+    void run() {
+        sc = new Scanner(System.in);
+        int veces = sc.nextInt();
+        for (int i = 0; i < veces; i++) {
+            caso();
+        }
+    }
+
+    public static void main(String[] args) {
+        new E355_GregorioXII().run();
+    }
+}
