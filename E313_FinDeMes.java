ea602a052e0a047248e623f818a6b9d6ec7516c6
ThePoisoned1 <60658745+ThePoisoned1@users.noreply.github.com>
2020-02-19 17:00:16 +0100

    Create E313_FinDeMes.java

---
+++ E313_FinDeMes.java
@@ -0,0 +1,28 @@
+package AceptaElReto;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author ThePoisoned1
+ */
+public class E313_FinDeMes {
+
+    Scanner sc;
+
+    void caso() {
+        System.out.println(sc.nextInt() + sc.nextInt() >= 0 ? "SI" : "NO");
+    }
+
+    void run() {
+        sc = new Scanner(System.in);
+        int veces = sc.nextInt();
+        for (int i = 0; i < veces; i++) {
+            caso();
+        }
+    }
+
+    public static void main(String[] args) {
+        new E313_FinDeMes().run();
+    }
+}
