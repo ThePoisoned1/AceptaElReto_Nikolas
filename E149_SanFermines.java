c548b01b32dd5e42640efbb13e9eed7511fff466
ThePoisoned1 <60658745+ThePoisoned1@users.noreply.github.com>
2020-02-19 17:34:23 +0100

    Create E149_SanFermines.java

---
+++ E149_SanFermines.java
@@ -0,0 +1,38 @@
+package AceptaElReto;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author ThePoisoned1
+ */
+public class E149_SanFermines {
+
+    Scanner sc;
+
+    boolean caso() {
+        if (!sc.hasNext()) {
+            return false;
+        }
+        int numToros = sc.nextInt();
+        int velocidadToro, velocidadPersona = 0;
+        for (int i = 0; i < numToros; i++) {
+            velocidadToro = sc.nextInt();
+            if (velocidadToro > velocidadPersona) {
+                velocidadPersona = velocidadToro;
+            }
+        }
+        System.out.println(velocidadPersona);
+        return true;
+    }
+
+    void run() {
+        sc = new Scanner(System.in);
+        while (caso()) {
+        }
+    }
+
+    public static void main(String[] args) {
+        new E149_SanFermines().run();
+    }
+}
