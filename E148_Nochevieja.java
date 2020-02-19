1b69dbddd31a88d7496ba7df5456a2667c3c07c1
ThePoisoned1 <60658745+ThePoisoned1@users.noreply.github.com>
2020-02-19 17:09:01 +0100

    Create E148_Nochevieja.java

---
+++ E148_Nochevieja.java
@@ -0,0 +1,33 @@
+package AceptaElReto;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author ThePoisoned1
+ */
+public class E148_Nochevieja {
+
+    Scanner sc;
+
+    boolean caso() {
+        String[] horaMin = sc.next().split(":");
+        int hora = Integer.parseInt(horaMin[0]);
+        int minuto = Integer.parseInt(horaMin[1]);
+        if (hora == 0 && minuto == 0) {
+            return false;
+        }
+        System.out.println(((23 - hora) * 60) + (60 - minuto));
+        return true;
+    }
+
+    void run() {
+        sc = new Scanner(System.in);
+        while (caso()) {
+        }
+    }
+
+    public static void main(String[] args) {
+        new E148_Nochevieja().run();
+    }
+}
