116fe97b39bfd66079326716927bea4bc13dedc3
ThePoisoned1 <60658745+ThePoisoned1@users.noreply.github.com>
2020-02-21 00:47:34 +0100

    Create E217_QueLadoDeLaCalle.java

---
+++ E217_QueLadoDeLaCalle.java
@@ -0,0 +1,31 @@
+package AceptaElReto;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author ThePoisoned1
+ */
+public class E217_QueLadoDeLaCalle {
+
+    Scanner sc;
+
+    boolean caso() {
+        int vivienda = sc.nextInt();
+        if (vivienda == 0) {
+            return false;
+        }
+        System.out.println(vivienda % 2 == 0 ? "DERECHA" : "IZQUIERDA");
+        return true;
+    }
+
+    void run() {
+        sc = new Scanner(System.in);
+        while (caso()) {
+        }
+    }
+
+    public static void main(String[] args) {
+        new E217_QueLadoDeLaCalle().run();
+    }
+}
