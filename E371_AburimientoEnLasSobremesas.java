6f555e30d5eb8497c20fe91556ec1683f614c09d
ThePoisoned1 <60658745+ThePoisoned1@users.noreply.github.com>
2020-02-21 00:55:58 +0100

    Create E371_AburimientoEnLasSobremesas.java

---
+++ E371_AburimientoEnLasSobremesas.java
@@ -0,0 +1,31 @@
+package AceptaElReto;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author ThePoisoned1
+ */
+public class E371_AburimientoEnLasSobremesas {
+
+    Scanner sc;
+
+    boolean caso() {
+        int lado = sc.nextInt();
+        if (lado == 0) {
+            return false;
+        }
+        System.out.println((lado * (lado + 1) / 2) * 3);
+        return true;
+    }
+
+    void run() {
+        sc = new Scanner(System.in);
+        while (caso()) {
+        }
+    }
+
+    public static void main(String[] args) {
+        new E371_AburimientoEnLasSobremesas().run();
+    }
+}
