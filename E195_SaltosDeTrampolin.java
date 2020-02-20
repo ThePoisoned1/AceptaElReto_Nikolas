0a34fb979d5c25ff147e6a57bca6cc6cfa5aa860
ThePoisoned1 <60658745+ThePoisoned1@users.noreply.github.com>
2020-02-20 18:57:59 +0100

    Create E195_SaltosDeTrampolin.java

---
+++ E195_SaltosDeTrampolin.java
@@ -0,0 +1,41 @@
+package AceptaElReto;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+/**
+ *
+ * @author ThePoisoned1
+ */
+public class E195_SaltosDeTrampolin {
+
+    Scanner sc;
+
+    boolean caso() {
+        if (!sc.hasNext()) {
+            return false;
+        }
+        double[] puntuaciones = new double[7];
+        for (int i = 0; i < 7; i++) {
+            puntuaciones[i] = Double.parseDouble(sc.next());
+        }
+        Arrays.sort(puntuaciones);
+        double puntuacion = (puntuaciones[2] + puntuaciones[3] + puntuaciones[4]) * 2;
+        if (puntuacion == (int) puntuacion) {
+            System.out.println((int) puntuacion);
+        } else {
+            System.out.println((int) puntuacion++);
+        }
+        return true;
+    }
+
+    void run() {
+        sc = new Scanner(System.in);
+        while (caso()) {
+        }
+    }
+
+    public static void main(String[] args) {
+        new E195_SaltosDeTrampolin().run();
+    }
+}
