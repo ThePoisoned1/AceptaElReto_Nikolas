4af9a88ea18554a7851fea68b2243ac4953a914d
ThePoisoned1 <60658745+ThePoisoned1@users.noreply.github.com>
2020-02-20 03:22:21 +0100

    Create E369_ContandoenLaArena.java

---
+++ E369_ContandoenLaArena.java
@@ -0,0 +1,33 @@
+package AceptaElReto;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author ThePoisoned1
+ */
+public class E369_ContandoenLaArena {
+        Scanner sc;  
+
+    boolean caso() {
+        int numero=sc.nextInt();
+        if (numero==0) {
+            return false;
+        }
+        for (int i = 0; i < numero; i++) {
+            System.out.print(1);
+        }
+        System.out.println("");
+        return true;
+    }
+
+    void run() {
+        sc = new Scanner(System.in);
+        while (caso()) {
+        }
+    }
+
+    public static void main(String[] args) {
+        new E369_ContandoenLaArena().run();
+    }
+}
