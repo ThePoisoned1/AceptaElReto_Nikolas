3a39eb7182a2a34a920a4a5b125233a1dda0c738
ThePoisoned1 <60658745+ThePoisoned1@users.noreply.github.com>
2020-03-09 20:54:09 +0100

    Create E179_MolinosAlViento.java

---
+++ E179_MolinosAlViento.java
@@ -0,0 +1,42 @@
+package AceptaElReto;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author ThePoisoned1
+ */
+public class E179_MolinosAlViento {
+
+    Scanner sc;
+
+    boolean caso() {
+        int cantMolinos = sc.nextInt();
+        if (cantMolinos == 0) {
+            return false;
+        }
+        int[] energiaMolinos = new int[cantMolinos + 1];
+        energiaMolinos[0] = 0;
+        for (int i = 1; i <= cantMolinos; i++) {
+            energiaMolinos[i] = energiaMolinos[i - 1] + sc.nextInt();
+        }
+        int numConsultas = sc.nextInt();
+        int inicio, fin;
+        for (int i = 0; i < numConsultas; i++) {
+            inicio = sc.nextInt();
+            fin = sc.nextInt();
+            System.out.println(energiaMolinos[fin] - energiaMolinos[inicio - 1]);
+        }
+        return true;
+    }
+
+    void run() {
+        sc = new Scanner(System.in);
+        while (caso()) {
+        }
+    }
+
+    public static void main(String[] args) {
+        new E179_MolinosAlViento().run();
+    }
+}
