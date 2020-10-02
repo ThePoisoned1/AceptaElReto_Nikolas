7fdd6c7cc7120cf6548bcf8d3fedb24a52b3a461
ThePoisoned1 <60658745+ThePoisoned1@users.noreply.github.com>
2020-10-02 14:36:24 +0200

    Create E438_Esgritura.java

---
+++ E438_Esgritura.java
@@ -0,0 +1,37 @@
+package AceptaElReto.AC;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author ThePoisoned1
+ */
+public class E438_Esgritura {
+
+    Scanner sc = new Scanner(System.in);
+
+    boolean caso() {
+        if (!sc.hasNext()) {
+            return false;
+        }
+        int numLetras = 0, numEx = 0;
+        String mensaje = sc.nextLine();
+        for (int i = 0; i < mensaje.length(); i++) {
+            if (Character.isLetter(mensaje.charAt(i))) {
+                numLetras++;
+            } else if (mensaje.charAt(i) == '!') {
+                numEx++;
+            }
+        }
+        System.out.println(numEx > numLetras ? "ESGRITO" : "escrito");
+        return true;
+    }
+
+    void run() {
+        while (caso());
+    }
+
+    public static void main(String[] args) {
+        new E438_Esgritura().run();
+    }
+}
