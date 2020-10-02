27fdc40fef3afe74f69cc907a4e637cd45b45403
ThePoisoned1 <60658745+ThePoisoned1@users.noreply.github.com>
2020-10-02 14:42:09 +0200

    Create E440_EscalandoElEverest.java

---
+++ E440_EscalandoElEverest.java
@@ -0,0 +1,54 @@
+package AceptaElReto;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author ThePoisoned1
+ */
+public class E440_EscalandoElEverest {
+
+    Scanner sc;
+
+    boolean caso() {
+        int numEscaladores = sc.nextInt();
+        if (numEscaladores == 0) {
+            return false;
+        }
+        int numGrupos = 1, grupoMax = 0, grupoMin = 10001, grupo = 1;
+        long v, vPrimero = sc.nextLong();
+        for (int i = 1; i < numEscaladores; i++) {
+            v = sc.nextLong();
+            if (v < vPrimero) {
+                numGrupos++;
+                if (grupo > grupoMax) {
+                    grupoMax = grupo;
+                }
+                if (grupo < grupoMin) {
+                    grupoMin = grupo;
+                }
+                grupo = 1;
+                vPrimero = v;
+            } else {
+                grupo++;
+            }
+        }
+        if (grupo > grupoMax) {
+            grupoMax = grupo;
+        }
+        if (grupo < grupoMin) {
+            grupoMin = grupo;
+        }
+        System.out.println(numGrupos + " " + grupoMin + " " + grupoMax);
+        return true;
+    }
+
+    void run() {
+        sc = new Scanner(System.in);
+        while (caso());
+    }
+
+    public static void main(String[] args) {
+        new E440_EscalandoElEverest().run();
+    }
+}
